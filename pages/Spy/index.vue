a0010f6c0051d32de3e330ef4c0a0fe6c0c7df5c
sylvia <1391847132@qq.com>
2021-02-04 16:49:16 +0800

    spy的一些修改

---
+++ pages/Spy/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class="seat">
+           <seat></seat> 
+        </div>
+    </div>
+</template>
+
+<script>
+    import Seat from '../../components/waitSeat'
+    export default {
+        name:"Spy",
+        components:{
+            Seat
+        }
+    }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
