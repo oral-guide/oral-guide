6b38d3d312e1d5f42372eac31370bf0c90c8763b
Will20001102 <2264836007@qq.com>
2021-02-06 20:33:09 +0800

    Spy页面两个简单的组件（有待完善）

---
+++ pages/Spy/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <vote></vote>
+        <word></word>
+    </div>
+</template>
+
+<script>
+import vote from '../../components/vote'
+import word from '../../components/word'
+
+export default {
+  name: 'spyRoom',
+  components: {
+    vote,
+    word
+  },
+//   data(){
+//       return {
+
+//       }
+//   },
+//   methods: {
+
+//   }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>>
