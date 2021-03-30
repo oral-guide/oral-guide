22fa1278649eadf703409024e9b40b1c81b40844
Will20001102 <2264836007@qq.com>
2021-03-30 16:47:11 +0800

    战绩

--- pages/History/index.vue
+++ pages/History/index.vue
@@ -14,12 +14,11 @@
       <van-tab title="Shadow Exercise" name="shadow">
         <div class="main">
           <!-- shadow game的战绩 -->
-          <div v-for="(s, index) in userInfo.history.shadow" :key="index">
-            {{ $util.formatTime(s.time) }}
-            获得了{{ s.exp }}
-            {{ s.result }}
+          <div v-for="(s, index) in shadowHistory" :key="index">
+            <div class="time"><span>Time:</span> {{ $util.formatTime(s.time) }}</div>
+            <div class="exp"><span>Exp:</span> {{ s.exp }}</div>
             <!-- 弄个组件 shadowResult.vue -->
-            <shadowResult :params="s.result[state]"></shadowResult>
+            <shadowResult :result="s.result[state]" :state="state" @exchange="exchange"></shadowResult>
           </div>
         </div>
       </van-tab>
@@ -35,19 +34,53 @@
 
 <script>
 import { mapState } from 'vuex';
+import shadowResult from "../../components/shadowResult.vue";
 export default {
+  components: {
+    shadowResult
+  },
   data() {
     return {
+      state: 0,
     }
   },
   computed: {
-    ...mapState(['userInfo'])
+    ...mapState(['userInfo']),
+    shadowHistory() {
+      return this.userInfo.history.shadow.slice().reverse();
+    }
+  },
+  methods: {
+    exchange() {
+      if(this.state == 0){
+        this.state = 1
+      } else {
+        this.state = 0
+      }
+      console.log(this.state);
+      
+    }
+  },
+  mounted () {
+    console.log(this.shadowHistory);
   },
 };
 </script>
 
 <style lang="scss" scoped>
 .main {
-  padding: 20px;
+  position: relative;
+  margin:10px;
+  padding: 10px;
+  border: 1px solid;
+  span{
+    font-weight: bold;
+  }
+  .exp {
+    position: absolute;
+    // right: 5px;
+  }
 }
+
+
 </style>
