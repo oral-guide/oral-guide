89cbe4e0f71c0413d83dd94fec8c717c12f71944
Will20001102 <2264836007@qq.com>
2021-03-30 13:29:05 +0800

    双人游戏句子标注

--- pages/2Player/index.vue
+++ pages/2Player/index.vue
@@ -2,19 +2,6 @@
   <div>
     <!-- 通知栏 -->
     <van-notice-bar color="#ff4101" left-icon="volume-o" :text="noticeText" />
-    <!-- 问题 -->
-    <!-- <div class="question">
-      <h2>Round {{ round + 1 }}</h2>
-      <template v-if="showResult">
-        <p class="sentence">{{ sentences[round].sentence }}</p>
-        <van-progress
-          class="score1"
-          :pivot-text="Fscore"
-          color="#40b883"
-          :percentage="percentage"
-        />
-      </template>
-    </div>-->
     <!-- 玩家自己 -->
     <div class="player1">
       <!-- 用户信息 -->
@@ -48,13 +35,67 @@
       <h1 class="round">Round {{ number + 1}}</h1>
       <!-- 句子和得分条 -->
       <div v-if="rated">
-        <p class="sentence">{{sentences[number].sentence}}</p>
-        <van-progress :pivot-text="player.scores[number]" color="#40b883" :percentage="player.scores[number]" stroke-width="4" />
+        <p class="sentence" v-html="sentence"></p>
+        <!-- <van-progress :pivot-text="player.scores[number]" color="#40b883" :percentage="player.scores[number]" stroke-width="4" /> -->
+        <div class="score">
+          Total score：
+          <van-progress
+            :pivot-text="player.scores[number].total_score"
+            color="#40b883"
+            :percentage="player.scores[number].total_score"
+            stroke-width="4"
+          />
+        </div>
+        <div class="score">
+          Accuracy score:
+          <van-progress
+            :pivot-text="player.scores[number].accuracy_score"
+            color="#40b883"
+            :percentage="player.scores[number].accuracy_score"
+            stroke-width="4"
+          />
+        </div>
+        <div class="score">
+          Fluency score:
+          <van-progress
+            :pivot-text="player.scores[number].fluency_score"
+            color="#40b883"
+            :percentage="player.scores[number].fluency_score"
+            stroke-width="4"
+          />
+        </div>
+        <div class="score">
+          Standard score:
+          <van-progress
+            :pivot-text="player.scores[number].standard_score"
+            color="#40b883"
+            :percentage="player.scores[number].standard_score"
+            stroke-width="4"
+          />
+        </div>
+        <div class="score">
+          Integrity score:
+          <van-progress
+            :pivot-text="player.scores[number].integrity_score"
+            color="#40b883"
+            :percentage="player.scores[number].integrity_score"
+            stroke-width="4"
+          />
+        </div>
       </div>
     </div>
 
     <!-- 结果 -->
-    <gameResult v-if="isEnded" :players="players" :sentences="sentences"></gameResult>
+    <!-- <gameResult v-if="isEnded" :players="players" :sentences="sentences"></gameResult> -->
+
+    <!-- 结果 -->
+    <gameResult
+      v-if="showResultDialog"
+      :players="players"
+      :sentences="resultSentences"
+      :urls="urls"
+      @retry="retry"
+    ></gameResult>
 
     <van-popup :show="showRecordingDialog" :close-on-click-overlay="false" position="bottom">
       <van-button color="#ff6600" block @click="stopRecord">Stop recording {{ timerCount }}s</van-button>
@@ -87,7 +128,10 @@ export default {
       timer: null,
       isEnded: false,
       noticeText: "",
-      rated: false
+      rated: false,
+      sentence: "", // 标注的句子
+      resultSentences: [], // 传给result组件的sentences
+      showResultDialog: false, //结果弹框
     };
   },
   computed: {
@@ -98,10 +142,13 @@ export default {
     },
     opponentTotalScore() {
       return Math.ceil(this.opponent.scores.reduce(this.sum, 0) / 5);
+    },
+    urls() {
+      return this.sentences.map(s => s.audioUrl);
     }
   },
   methods: {
-    sum: (a, b) => a + b,
+    sum: (a, b) => a + b.total_score,
     preparing() {
       this.noticeText = "Preparing stage";
       Toast({
@@ -136,6 +183,34 @@ export default {
         }
       }, 1000);
     },
+    //重新录音
+    retry(index) {
+      this.number = index;
+      console.log(this.number);
+      console.log("重新开始录音");
+      // this.showResultDialog = false;
+      this.showRecordingDialog = true;
+      recorderManager.start({
+        duration: 10000,
+        format: "mp3",
+        sampleRate: 16000,
+        numberOfChannels: 1,
+      });
+      Toast({
+        duration: 0,
+        message: "Recording...",
+        selector: "#van-toast",
+      });
+      this.timerCount = 10;
+      let timer = setInterval(() => {
+        this.timerCount--;
+        if (this.timerCount == 0) {
+          // 时间到，强制结束录音并上传
+          clearInterval(timer);
+          this.stopRecord();
+        }
+      }, 1000);
+    },
     // 结束录音
     stopRecord() {
       Toast.clear();
@@ -149,11 +224,12 @@ export default {
         if (n < 5) {
           Toast.clear();
           this.rated = true;
+          this.noticeText = "Rating stage";
           setTimeout(() => {
             this.rated = false;
             this.preparing();
             this.number ++;
-          }, 3000);
+          }, 5000);
         } else {
           this.isEnded = true;
         }
@@ -174,14 +250,64 @@ export default {
         res.tempFilePath,
         this.sentences[this.round].sentence
       );
-      let { score, audioSrc } = JSON.parse(data.data); //获取打分api的分数
-      this.score = Math.ceil(score / 5); //转成20分制
-    //   this.rated = true;
-      this.$util.updateGamePlayers(score, audioSrc);
-      Toast({
-        message: "waiting for the other player",
-        duration: 0
+      let {
+        result: {
+          integrity_score,
+          sentence: {
+            accuracy_score,
+            fluency_score,
+            standard_score,
+            total_score,
+            word,
+          },
+        },
+        audioSrc,
+      } = JSON.parse(data.data); //获取打分api的分数
+      accuracy_score = Math.ceil(accuracy_score * 20); // 准确度
+      fluency_score = Math.ceil(fluency_score * 20); // 流畅度
+      standard_score = Math.ceil(standard_score * 20); // 标准度
+      integrity_score = Math.ceil(integrity_score * 20); // 完整度
+      total_score = Math.ceil(total_score * 20); // 总分
+      let words = word.filter((w) => w.total_score); // 单词数组
+
+      let sentence = "";
+      words.forEach((w, index) => {
+        if (w.total_score > 4.5) {
+          sentence = `${sentence}<span style="color: #40b883">${w.content}</span> `;
+        } else if (w.total_score < 3) {
+          sentence = `${sentence}<span style="color: tomato">${w.content}</span> `;
+        } else {
+          sentence = `${sentence}${w.content} `;
+        }
+        if (index === words.length - 1) {
+          sentence = sentence.trim() + ".";
+        }
       });
+      if (!this.isEnded) {  
+        this.sentence = sentence[0].toUpperCase() + sentence.slice(1);
+        this.score = Math.ceil(total_score / 5); //转成20分制
+        this.$util.updateGamePlayers({
+          accuracy_score,
+          fluency_score,
+          standard_score,
+          integrity_score,
+          total_score,
+        }, audioSrc);
+        Toast({
+          message: "waiting for the other player",
+          duration: 0
+        });
+      } else {
+        this.player.scores[this.number] = {
+          accuracy_score,
+          fluency_score,
+          standard_score,
+          integrity_score,
+          total_score,
+        };
+        this.player.recordings[this.number] = audioSrc;
+        this.resultSentences[this.number] = sentence;
+      }
     });
   }
 };
@@ -190,14 +316,14 @@ export default {
 <style lang="scss" scoped>
 .question {
   position: relative;
-  margin: 5% 5%;
+  margin: 2.5% 5%;
   padding: 5% 5%;
   border: 1px solid;
-  height: 25vh;
+  height: 50vh;
   text-align: center;
   background-color: burlywood;
   .sentence {
-    font-size: x-large;
+    // font-size: x-large;
     margin-bottom: 10px;
   }
 }
@@ -205,8 +331,8 @@ export default {
 .player1,
 .player2 {
   position: relative;
-  margin: 5% 5%;
-  padding: 5% 5%;
+  margin: 2.5% 5%;
+  padding: 2.5% 5%;
   border: 1px solid;
   .user {
     display: flex;
@@ -214,8 +340,8 @@ export default {
     // margin-left:10px;
     margin-bottom: 20px;
     img {
-      width: 18vw;
-      height: 18vw;
+      width: 10vw;
+      height: 10vw;
       border-radius: 50%;
     }
     .name {
