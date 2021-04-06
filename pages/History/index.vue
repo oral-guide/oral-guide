eddbc7647d24b228b66a0dfdf0b544197500d8ed
Will20001102 <2264836007@qq.com>
2021-04-06 22:08:27 +0800

    spy战绩

--- pages/History/index.vue
+++ pages/History/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <van-tabs
-      active="shadow"
+      :active="game"
       sticky
       animated
       swipeable
@@ -10,22 +10,35 @@
       line-width="80px"
       color="#ff4101"
       title-active-color="#ff4101"
+      @change="handleChange"
     >
       <van-tab title="Shadow Exercise" name="shadow">
-        <div class="main">
-          <!-- shadow game的战绩 -->
-          <div class="history" v-for="(s, index) in shadowHistory" :key="index">
-            <div class="time">{{ $util.formatTime(s.time) }}</div>
-            <div class="exp">Exp: {{ s.exp }}</div>
-            <!-- 弄个组件 shadowResult.vue -->
-            <shadowResult :result="s.result"></shadowResult>
+        <template v-if="game === 'shadow'">
+          <div v-if="shadowHistory.length > 0" class="main">
+            <!-- shadow game的战绩 -->
+            <div class="history" v-for="(s, index) in shadowHistory" :key="index">
+              <div class="time">{{ $util.formatTime(s.time) }}</div>
+              <div class="exp">Exp: {{ s.exp }}</div>
+              <!-- 弄个组件 shadowResult.vue -->
+              <shadowResult :result="s.result"></shadowResult>
+            </div>
           </div>
-        </div>
+          <van-empty v-else description="No history" />
+        </template>
       </van-tab>
       <van-tab title="Who is the spy" name="spy">
-        <div class="main">
-          <!-- spy game的战绩 -->
-        </div>
+        <template v-if="game === 'spy'">
+          <div v-if="spyHistory.length > 0" class="main">
+            <!-- spy game的战绩 -->
+            <div class="history" v-for="(s, index) in spyHistory" :key="index">
+              <div class="time">{{ $util.formatTime(s.time) }}</div>
+              <div class="exp">Exp: {{ s.exp }}</div>
+              <!-- 弄个组件 spyResult.vue -->
+              <spyResult :result="s.result[0]" :word="s.word"></spyResult>
+            </div>
+          </div>
+          <van-empty v-else description="No history" />
+        </template>
       </van-tab>
     </van-tabs>
   </div>
@@ -34,13 +47,15 @@
 <script>
 import { mapState } from "vuex";
 import shadowResult from "../../components/shadowResult.vue";
+import spyResult from "../../components/spyResult.vue";
 export default {
   components: {
     shadowResult,
+    spyResult
   },
   data() {
     return {
-      state: 0,
+      game: "shadow"
     };
   },
   computed: {
@@ -48,20 +63,19 @@ export default {
     shadowHistory() {
       return this.userInfo.history.shadow.slice().reverse();
     },
+    spyHistory() {
+      return this.userInfo.history.spy.slice().reverse();
+    }
   },
   methods: {
-    // exchange() {
-    //   if(this.state == 0){
-    //     this.state = 1
-    //   } else {
-    //     this.state = 0
-    //   }
-    //   console.log(this.state);
-    // }
+    handleChange({ detail: { name }}) {
+      console.log(name);
+      this.game = name;
+    }
   },
   mounted() {
     console.log(this.shadowHistory);
-  },
+  }
 };
 </script>
 
